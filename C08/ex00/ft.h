1c6dddd3ac136a7ba83f790eb68c5558c6af59aa
Gihyun Cho <gicho@c4r1s1.42seoul.kr>
2020-01-31 16:03:19 +0900

    go

---
+++ C08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/31 14:44:52 by gicho             #+#    #+#             */
+/*   Updated: 2020/01/31 14:57:21 by gicho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/31 15:29:10 by gicho             #+#    #+#             */
+/*   Updated: 2020/01/31 16:03:07 by gicho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int					get_str_size(char *str)
+{
+	int ret;
+
+	ret = 0;
+	while (str[ret])
+		++ret;
+	return (ret);
+}
+
+void				str_cpy(char *dst, char *src)
+{
+	while (*src)
+		*(dst++) = *(src++);
+	*dst = 0;
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str *ret;
+	int			i;
+
+	ret = (t_stock_str*)malloc(sizeof(t_stock_str) * ac);
+	if (!ret)
+		return (0);
+	i = 0;
+	while (i < ac)
+	{
+		ret[i].size = get_str_size(av[i]);
+		ret[i].str = (char*)malloc(ret[i].size + 1);
+		ret[i].copy = (char*)malloc(ret[i].size + 1);
+		str_cpy(ret[i].str, *av);
+		str_cpy(ret[i].copy, *av);
+		++av;
+	}
+	return (ret);
+}
