4601df7e7031669628cd20ddc2530cef7ae45aeb
Gihyun Cho <gicho@c4r1s1.42seoul.kr>
2020-01-31 16:21:59 +0900

    complete

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 15:29:10 by gicho             #+#    #+#             */
-/*   Updated: 2020/01/31 16:03:07 by gicho            ###   ########.fr       */
+/*   Updated: 2020/01/31 16:20:50 by gicho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 	t_stock_str *ret;
 	int			i;
 
-	ret = (t_stock_str*)malloc(sizeof(t_stock_str) * ac);
+	ret = (t_stock_str*)malloc(sizeof(t_stock_str) * (ac + 1));
 	if (!ret)
 		return (0);
 	i = 0;
@@ -43,9 +43,10 @@ struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 		ret[i].size = get_str_size(av[i]);
 		ret[i].str = (char*)malloc(ret[i].size + 1);
 		ret[i].copy = (char*)malloc(ret[i].size + 1);
-		str_cpy(ret[i].str, *av);
-		str_cpy(ret[i].copy, *av);
-		++av;
+		str_cpy(ret[i].str, av[i]);
+		str_cpy(ret[i].copy, av[i]);
+		++i;
 	}
+	ret[i].str = 0;
 	return (ret);
 }
---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/31 16:21:17 by gicho             #+#    #+#             */
+/*   Updated: 2020/01/31 16:21:42 by gicho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_size(int size)
+{
+	char c;
+
+	if (!size)
+		return ;
+	c = '0' + size % 10;
+	print_size(size / 10);
+	write(1, &c, 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		write(1, par[i].str, par[i].size);
+		write(1, "\n", 1);
+		print_size(par[i].size);
+		write(1, "\n", 1);
+		write(1, par[i].copy, par[i].size);
+		write(1, "\n", 1);
+		++i;
+	}
+}
