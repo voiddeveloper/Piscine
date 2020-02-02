5e8c8f7d98f3e25613da27044daabb577048d46c
Gihyun Cho <gicho@c4r2s2.42seoul.kr>
2020-02-02 18:32:15 +0900

    ha

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -6,7 +6,7 @@
 /*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 16:21:17 by gicho             #+#    #+#             */
-/*   Updated: 2020/02/02 18:26:26 by gicho            ###   ########.fr       */
+/*   Updated: 2020/02/02 18:31:36 by gicho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,7 @@ void	print_size(int size)
 	char c;
 
 	if (!size)
-	{
-		write(1, "0", 1);
 		return ;
-	}
 	c = '0' + size % 10;
 	print_size(size / 10);
 	write(1, &c, 1);
@@ -36,7 +33,10 @@ void	ft_show_tab(struct s_stock_str *par)
 	{
 		write(1, par[i].str, par[i].size);
 		write(1, "\n", 1);
-		print_size(par[i].size);
+		if (par[i].size)
+			print_size(par[i].size);
+		else
+			write(1, "0", 1);
 		write(1, "\n", 1);
 		write(1, par[i].copy, par[i].size);
 		write(1, "\n", 1);
