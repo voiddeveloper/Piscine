ec1367b10b899451423df2092d2793b4dbc2ddbf
Gihyun Cho <gicho@c3r8s5.42seoul.kr>
2020-02-04 22:34:59 +0900

    modified

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,7 +6,7 @@
 /*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 22:26:52 by gicho             #+#    #+#             */
-/*   Updated: 2020/02/04 20:43:36 by gicho            ###   ########.fr       */
+/*   Updated: 2020/02/04 22:34:49 by gicho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 			continue;
 		if (k == 0)
 			k = tmp;
-		else if (k != tmp)
+		else if ((long long)k * tmp < 0)
 			return (0);
 	}
 	return (1);
