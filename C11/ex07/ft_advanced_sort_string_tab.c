30d751eca2b12dbe7c5fed7284ba87dbffc31802
Gihyun Cho <gicho@c3r8s5.42seoul.kr>
2020-02-04 18:00:10 +0900

    go

---
+++ C11/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/04 17:59:46 by gicho             #+#    #+#             */
+/*   Updated: 2020/02/04 17:59:55 by gicho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_get_tab_size(char **tab)
+{
+	int ret;
+
+	ret = 0;
+	while (tab[ret])
+		++ret;
+	return (ret);
+}
+
+void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
+{
+	int		i;
+	int		j;
+	int		size;
+	char	*tmp;
+
+	size = ft_get_tab_size(tab);
+	i = 1;
+	while (i++ < size)
+	{
+		j = 0;
+		while (++j < size)
+		{
+			if (cmp(tab[j - 1], tab[j]) < 0)
+			{
+				tmp = tab[j - 1];
+				tab[j - 1] = tab[j];
+				tab[j] = tmp;
+			}
+		}
+	}
+}
