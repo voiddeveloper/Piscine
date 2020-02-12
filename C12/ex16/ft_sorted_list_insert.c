b34bc382666a6b46445d18f8d5454e7703da6c50
Gihyun Cho <gicho@c3r8s2.42seoul.kr>
2020-02-12 11:56:47 +0900

    hum

--- C12/ex16/ft_sorted_list_insert.c
+++ C12/ex16/ft_sorted_list_insert.c
@@ -6,7 +6,34 @@
 /*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 23:29:12 by gicho             #+#    #+#             */
-/*   Updated: 2020/02/11 23:31:27 by gicho            ###   ########.fr       */
+/*   Updated: 2020/02/12 11:32:14 by gicho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
+{
+	t_list *prev;
+	t_list *curr;
+
+	if (!*begin_list)
+	{
+		*begin_list = ft_create_elem(data);
+		return ;
+	}
+	if (cmp((*begin_list)->data, data) > 0)
+	{
+		curr = *begin_list;
+		*begin_list = ft_create_elem(data);
+		(*begin_list)->next = curr;
+		return ;
+	}
+	prev = *begin_list;
+	curr = prev->next;
+	while (curr && (cmp(curr->data, data) < 0))
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+	prev->next = ft_create_elem(data);
+	prev->next->next = curr;
+}
---
+++ C12/ex17/ft_sorted_list_merge.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sorted_list_merge.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gicho <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/12 11:33:27 by gicho             #+#    #+#             */
+/*   Updated: 2020/02/12 11:56:34 by gicho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_list *g_head;
+t_list *g_l;
+t_list *g_r;
+t_list *g_tmp;
+
+void	init(t_list *begin_list1, t_list *begin_list2, int (*cmp)())
+{
+	if (cmp(begin_list1->data, begin_list2->data) > 0)
+	{
+		g_l = begin_list1;
+		g_r = begin_list2->next;
+		g_head = begin_list2;
+	}
+	else
+	{
+		g_l = begin_list1->next;
+		g_r = begin_list2;
+		g_head = begin_list1;
+	}
+	g_tmp = g_head;
+}
+
+void	ft_sorted_list_merge(t_list **begin_list1, t_list *begin_list2,
+		int (*cmp)())
+{
+	if (!*begin_list1 || !begin_list2)
+		return ;
+	init(*begin_list1, begin_list2, cmp);
+	*begin_list1 = g_head;
+	while (g_l && g_r)
+	{
+		if (cmp(g_l->data, g_r->data) > 0)
+		{
+			g_tmp->next = g_r;
+			g_r = g_r->next;
+		}
+		else
+		{
+			g_tmp->next = g_l;
+			g_l = g_l->next;
+		}
+		g_tmp = g_tmp->next;
+	}
+	if (g_l)
+		g_tmp->next = g_l;
+	if (g_r)
+		g_tmp->next = g_r;
+}
